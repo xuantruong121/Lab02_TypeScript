22db0f437456b51f554085388dc57c13762f8d99
xuan truong <22714311.truong@student.iuh.edu.vn>
2025-08-30 14:27:37 +0700

    nop bai 14

---
+++ lab02/dist/B_14.js
@@ -0,0 +1,13 @@
+"use strict";
+// 14. Write an async function that takes a number, waits 1 second, and returns the number × 3.
+async function tripleAfter1Sec(num) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(num * 3);
+        }, 1000); // 1 second delay
+    });
+}
+(async () => {
+    const result = await tripleAfter1Sec(5);
+    console.log("Result:", result); // Output after 1s → 15
+})();
