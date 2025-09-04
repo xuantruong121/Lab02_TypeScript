ea2d9a7966a493d4bdbed1c51e76871cb05b76ee
Kennnguyen121 <22714311.truong@student.iuh.edu.vn>
2025-09-05 00:24:45 +0700

    nop cau 27 va 28

---
+++ lab02/dist/C_27.js
@@ -0,0 +1,33 @@
+"use strict";
+// 27. Write a function fetchWithRetry(url, retries) that retries up to retries times if
+// the API call fails.
+async function fetchWithRetry(url, retries) {
+    for (let attempt = 1; attempt <= retries; attempt++) {
+        try {
+            console.log(`Attempt ${attempt}...`);
+            const response = await fetch(url);
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            const data = await response.json();
+            console.log("Success!");
+            return data;
+        }
+        catch (error) {
+            console.error(`Attempt ${attempt} failed:`, error);
+            if (attempt === retries) {
+                throw new Error("All retries failed.");
+            }
+            await new Promise((resolve) => setTimeout(resolve, 1000));
+        }
+    }
+}
+(async () => {
+    try {
+        const data = await fetchWithRetry("https://jsonplaceholder.typicode.com/todos/1", 3);
+        console.log("Data:", data);
+    }
+    catch (error) {
+        console.error("Final Error:", error);
+    }
+})();
---
+++ lab02/dist/C_28.js
@@ -0,0 +1,24 @@
+"use strict";
+// 28. Write an async function batchProcess() that processes 5 async tasks at once (use
+// Promise.all).
+function asyncTask(id, time) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(`Task ${id} done after ${time}ms`);
+        }, time);
+    });
+}
+async function batchProcess() {
+    console.log("Start processing 5 tasks in parallel...");
+    const tasks = [
+        asyncTask(1, 1000),
+        asyncTask(2, 2000),
+        asyncTask(3, 1500),
+        asyncTask(4, 3000),
+        asyncTask(5, 2500),
+    ];
+    const results = await Promise.all(tasks);
+    console.log("All tasks completed!");
+    results.forEach((result) => console.log(result));
+}
+batchProcess();
