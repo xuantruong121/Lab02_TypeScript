327dd4ca87111afde18e4426d670324bd49bc092
Kennnguyen121 <22714311.truong@student.iuh.edu.vn>
2025-09-04 23:52:03 +0700

    nop bai C cau 21

--- lab02/dist/C_21.js
+++ lab02/dist/C_21.js
@@ -1,19 +1,17 @@
+"use strict";
 // 21. Use fetch to get data from a public API (e.g.,
 // https: //jsonplaceholder. typicode.com/todos/1).
 async function getTodo() {
     try {
         const response = await fetch("https://jsonplaceholder.typicode.com/todos/1");
-
         if (!response.ok) {
             throw new Error(`HTTP error! Status: ${response.status}`);
         }
-
         const todo = await response.json();
         console.log("Fetched todo: ", todo);
-
-    } catch (err) {
+    }
+    catch (err) {
         console.log("Fetch error: ", err);
     }
 }
-
 getTodo();
